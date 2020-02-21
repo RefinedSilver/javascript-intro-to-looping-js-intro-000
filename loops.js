32f47607c762a7cd9a97b3778b2d1c446f8c9117
Ian McKenzie <imckenzie777@gmail.com>
2020-02-21 09:13:31 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,9 @@
 function forLoop(arr) {
 
   for (let i = 0; i < arr.length; i++) {
+    if (i === 1) {
+      return `I am {arr[1]} strange loop.`;
+    }
     arr.push("Thanks, Flatiron for starting my programming career!")
   }
 }
