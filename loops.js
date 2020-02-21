c235d85c23c15c8f0324a0fd4e4c86e9cdb804a1
Ian McKenzie <imckenzie777@gmail.com>
2020-02-21 09:58:55 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,13 +1,13 @@
 function forLoop(arr) {
 	output = [];
 
-  for (let i = 0; i <= arr; i++) {
+  for (let i = 0; i < arr; i++) {
     if (i === 0) {
     	output.shift();
     } else if (i === 1) {
-      output.push("I am " + [i] + " strange loop.");
+      output.push("I am " + i + " strange loop.");
     } else {
-      output.push("I am " + [i] + " strange loops.");
+      output.push("I am " + i + " strange loops.");
     }
   }
   return output;
